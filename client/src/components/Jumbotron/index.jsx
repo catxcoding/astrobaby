b0a4728f64cd4670084a7c1ce8943f91ff30487b
unknown <jaclynnburch@gmail.com>
2024-03-17 16:00:20 -0700

    adding component files

---
+++ client/src/components/Jumbotron/index.jsx
@@ -0,0 +1,18 @@
+// 28-stu-mini > client > src > components > jumbotron > index
+
+function Jumbotron({ children }) {
+    return (
+        <div
+            style={{
+                height: 560,
+                clear: "both",
+                paddingTop: 120,
+                textAlign: "center",
+            }}
+        >
+            {children}
+        </div>
+    );
+}
+
+export default Jumbotron;
