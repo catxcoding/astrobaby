18ca6731fc89087c4dee73250a4616518d23803c
Aserrano7 <serranoandres022@gmail.com>
2024-03-18 21:42:07 -0700

    fixed some bugs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import Home from './pages/home';
+import Login from './pages/login';
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true, 
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },]}])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      ReactDOM.render(
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>,
+        document.getElementById("root")
+      );
