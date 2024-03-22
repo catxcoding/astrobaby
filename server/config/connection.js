75b3470a1bf1c5ec0ce0bfc8a2e5626af36aa28c
catxcoding <145952390+catxcoding@users.noreply.github.com>
2024-03-22 10:48:18 -0700

    updated mongoDB address

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/astro-baby');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/astro-baby');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://turbobaby:<o9WGdxEItnP94Mec>@cluster0.djrwp59.mongodb.net/');
 
 module.exports = mongoose.connection;
