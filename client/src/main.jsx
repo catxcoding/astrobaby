0ae1d0f936d0725457f187646fba4203c9fbe8dc
catxcoding <145952390+catxcoding@users.noreply.github.com>
2024-03-25 14:56:46 -0700

    Edits to deploy on render

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,36 +1,41 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Home from './pages/home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Detail from './pages/Detail';
-import OrderHistory from './pages/OrderHistory';
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./pages/home.jsx";
+import Login from "./pages/login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Detail from "./pages/Detail.jsx";
+import OrderHistory from "./pages/orderHistory.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true, 
-        element: <Home />
-      },
-      {
-        path: '/login',
-        element: <Login />
-      },{
-        path: '/signup',
-        element: <Signup />
-      },{
-        path: '/orderHistory',
-        element: <OrderHistory />
-      }, {
-        path: '/products/:id',
-        element: <Detail />
-      }]}])
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+            {
+                path: "/orderHistory",
+                element: <OrderHistory />,
+            },
+            {
+                path: "/products/:id",
+                element: <Detail />,
+            },
+        ],
+    },
+]);
 
-    
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
 );
