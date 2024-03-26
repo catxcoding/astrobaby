270e4aee076298943002338a87d52b2887c1e872
unknown <jaclynnburch@gmail.com>
2024-03-26 06:56:17 -0700

    nav and category menu page color theme

--- client/src/components/CategoryMenu.jsx
+++ client/src/components/CategoryMenu.jsx
@@ -1,10 +1,80 @@
+// import { useEffect } from "react";
+// import { useQuery } from "@apollo/client";
+// import { useStoreContext } from "../utils/GlobalState";
+// import {
+//     UPDATE_CATEGORIES,
+//     UPDATE_CURRENT_CATEGORY,
+// } from "../utils/actions";
+// import { QUERY_CATEGORIES } from "../utils/queries";
+// import { idbPromise } from "../utils/helpers";
+// import { Typography, Button } from "@mui/material";
+
+// function CategoryMenu() {
+//     const [state, dispatch] = useStoreContext();
+
+//     const { categories } = state;
+
+//     const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+//     useEffect(() => {
+//         if (categoryData) {
+//             dispatch({
+//                 type: UPDATE_CATEGORIES,
+//                 categories: categoryData.categories,
+//             });
+//             categoryData.categories.forEach((category) => {
+//                 idbPromise("categories", "put", category);
+//             });
+//         } else if (!loading) {
+//             idbPromise("categories", "get").then((categories) => {
+//                 dispatch({
+//                     type: UPDATE_CATEGORIES,
+//                     categories: categories,
+//                 });
+//             });
+//         }
+//     }, [categoryData, loading, dispatch]);
+
+//     const handleClick = (id) => {
+//         dispatch({
+//             type: UPDATE_CURRENT_CATEGORY,
+//             currentCategory: id,
+//         });
+//     };
+
+// console.log(categories)
+
+//     return (
+//         <div>
+//             <Typography variant="h5">Choose a Category:</Typography>
+//             {categories.map((item) => (
+//                 <Button
+//                     key={item._id}
+//                     variant="contained"
+//                     color="primary"
+//                     onClick={() => handleClick(item._id)}
+//                     style={{ marginRight: 10, marginBottom: 10 }}
+//                 >
+//                     {item.name}
+//                 </Button>
+//             ))}
+//             <Button
+//                 variant="contained"
+//                 color="primary"
+//                 onClick={() => handleClick("")}
+//             >
+//                 All
+//             </Button>
+//         </div>
+//     );
+// }
+
+// export default CategoryMenu;
+
 import { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { useStoreContext } from "../utils/GlobalState";
-import {
-    UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY,
-} from "../utils/actions";
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from "../utils/actions";
 import { QUERY_CATEGORIES } from "../utils/queries";
 import { idbPromise } from "../utils/helpers";
 import { Typography, Button } from "@mui/material";
@@ -41,30 +111,41 @@ function CategoryMenu() {
             currentCategory: id,
         });
     };
-    
-console.log(categories)
+
+    console.log(categories);
 
     return (
         <div>
-            <Typography variant="h5">Choose a Category:</Typography>
+            <Typography
+                variant="h6"
+                style={{ marginBottom: 10, marginTop: 10 }}
+            >
+                Choose a Category:
+            </Typography>
             {categories.map((item) => (
                 <Button
                     key={item._id}
                     variant="contained"
-                    color="primary"
+                    style={{
+                        marginRight: 10,
+                        marginBottom: 10,
+                        backgroundColor: "#607d8b",
+                    }} // Baby pink color
                     onClick={() => handleClick(item._id)}
-                    style={{ marginRight: 10, marginBottom: 10 }}
                 >
                     {item.name}
                 </Button>
             ))}
             <Button
                 variant="contained"
-                color="primary"
+                style={{ backgroundColor: "#607d8b", marginBottom: 10 }} 
                 onClick={() => handleClick("")}
             >
                 All
             </Button>
+            <Typography variant="h4" style={{ marginBottom: 20 }}>
+                Our Products
+            </Typography>
         </div>
     );
 }
--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -56,9 +56,98 @@
 
 // export default Nav;
 
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+// Pink theme
+
+// import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+// import { Link } from "react-router-dom";
+// import Auth from "../../utils/auth";
+// import { createTheme } from "@mui/material/styles";
+
+
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#bbdefb',
+//     },
+//     secondary: {
+//       main: '#f48fb1',
+//     },
+//   },
+// });
+
+// function Nav() {
+//     function showNavigation() {
+//         if (Auth.loggedIn()) {
+//             return (
+//                 <Toolbar>
+//                     <Button component={Link} to="/shop" color="inherit">
+//                         Products
+//                     </Button>
+//                     <Button component={Link} to="/orderHistory" color="inherit">
+//                         Order History
+//                     </Button>
+//                     <Button color="inherit" onClick={() => Auth.logout()}>
+//                         Logout
+//                     </Button>
+//                 </Toolbar>
+//             );
+//         } else {
+//             return (
+//                 <Toolbar>
+//                     <Button component={Link} to="/signup" color="inherit">
+//                         Signup
+//                     </Button>
+//                     <Button component={Link} to="/login" color="inherit">
+//                         Login
+//                     </Button>
+//                 </Toolbar>
+//             );
+//         }
+//     }
+
+//     return (
+//         <AppBar
+//             position="static"
+//             style={{ backgroundColor: "#add8e6", width: "100%" }}
+//         >
+//             <Toolbar
+//                 style={{
+//                     fontFamily:
+//                         "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+//                     color: "white",
+//                 }}
+//             >
+//                 <Typography
+//                     variant="h2"
+//                     component={Link}
+//                     to="/"
+//                     style={{ flexGrow: 1, fontWeight: "bold" }}
+//                 >
+//                     <span role="img" aria-label="AstroBaby"></span>
+//                     AstroBaby
+//                 </Typography>
+//                 {showNavigation()}
+//             </Toolbar>
+//         </AppBar>
+//     );import React from 'react';
+
+
+import { AppBar, Toolbar, Typography, Button, ThemeProvider } from "@mui/material";
 import { Link } from "react-router-dom";
 import Auth from "../../utils/auth";
+import { createTheme } from "@mui/material/styles";
+// import astroLogo from "../../../public/images/astroLogo.jpg";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#bbdefb',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+});
 
 function Nav() {
     function showNavigation() {
@@ -91,30 +180,41 @@ function Nav() {
     }
 
     return (
-        <AppBar
-            position="static"
-            style={{ backgroundColor: "#add8e6", width: "100%" }}
-        >
-            <Toolbar
-                style={{
-                    fontFamily:
-                        "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
-                    color: "white",
-                }}
-            >
-                <Typography
-                    variant="h2"
-                    component={Link}
-                    to="/"
-                    style={{ flexGrow: 1, fontWeight: "bold" }}
+        <ThemeProvider theme={theme}>
+            <AppBar position="static" color="primary">
+                <Toolbar
+                    style={{
+                        fontFamily:
+                            "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
+                        color: theme.palette.common.white, // Using primary text color from the theme
+                    }}
                 >
-                    <span role="img" aria-label="AstroBaby"></span>
-                    AstroBaby
-                </Typography>
-                {showNavigation()}
-            </Toolbar>
-        </AppBar>
+                    <Typography
+                        variant="h2"
+                        component={Link}
+                        to="/"
+                        style={{
+                            flexGrow: 1,
+                            fontWeight: "bold",
+                            marginTop: 20,
+                            marginBottom: 20,
+                            color: "white",
+                                                }}
+                    >
+                        {" "}
+                        <span role="img" aria-label="AstroBaby">
+                            👶
+                        </span>
+                        AstroBaby
+                    </Typography>
+
+                    {showNavigation()}
+                </Toolbar>
+            </AppBar>
+        </ThemeProvider>
     );
 }
 
 export default Nav;
+
+
